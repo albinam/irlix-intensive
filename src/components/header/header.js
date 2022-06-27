1d202a3e86f42d9d7f1c5970ae66a7d86c4ab809
albinam <allbinosikm@gmail.com>
2022-06-27 16:16:40 +0400

    верстка главной страницы

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,6 +5,7 @@ import './header.scss';
 function Header() {
     return (
         <header className="header">
+            <div className="header__top">
             <div className="header__text">
                 <p className="header__title">Главная</p>
                 <p className="header__date">17 июля 2021</p>
@@ -14,6 +15,13 @@ function Header() {
                     <Logo/>
                 </a>
             </div>
+                </div>
+            <ul className='header__filter'>
+                <li className='header__filter-button'>Новинки</li>
+                <li  className='header__filter-button'>Сладкие</li>
+                <li className='header__filter-button'>Хит</li>
+                <li className='header__filter-button'>Крепкие</li>
+            </ul>
         </header>
     )
 }
--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -2,13 +2,24 @@ import React from 'react';
 import './main.scss';
 import Header from "../../components/header/header";
 import Footer from "../../components/footer/footer";
+import {drinks} from "../../assets/data/temporaryData";
+import Card from "../../components/card/card";
 
 function Main() {
     return (
         <div>
             <Header/>
             <main className="main">
-                <div className="container"/>
+                <div className="container">
+                    <section className='cards'>
+                        {drinks.map(drink => {
+                            return (
+                                <Card key={drink.id} image={drink.image} title={drink.title} degree={drink.degree}
+                                      subtitle={drink.subtitle}/>
+                            )
+                        })}
+                    </section>
+                </div>
             </main>
             <Footer/>
         </div>
